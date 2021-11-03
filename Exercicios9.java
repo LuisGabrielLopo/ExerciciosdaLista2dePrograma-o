f8ab372f8d68f8b683f07a25155ad79142342704
Luis <lglopo14@gmail.com>
2021-11-03 12:51:47 -0300

    Mostrando números em contagem de 1 a 300 em orde crescente e decrescente

---
+++ Exercicios9.java
@@ -0,0 +1,23 @@
+package ListaDeExercicios2;
+
+public class Exercicios9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numMenor = 1, numMaior = 300 ;
+		
+		System.out.println("Forma Crescente");
+		while(numMenor <= 300 ) {
+			System.out.println(numMenor);
+			numMenor += 1;
+		}
+		System.out.println("\nFORMA DESCRESCENTE");
+		while(numMaior >=1) {
+			System.out.println(numMaior);
+			numMaior-- ;
+		}
+
+
+	}
+
+}
